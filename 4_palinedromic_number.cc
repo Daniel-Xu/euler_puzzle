2b038820dad152d127a54c2b917fbc89d144c1f4
Daniel-Xu <xwd.forever@gmail.com>
2014-02-15 00:58:38 -0500

    remember to compare the max
    the first time I didn't compare the max
    
    and this is orignial version

---
+++ 4_palinedromic_number.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+bool isPaline(int src)
+{
+    int num = src;
+    int sum = 0;
+    while(num > 0) {
+        sum = sum * 10 + num%10;
+        num /= 10;
+    }
+    if(sum == src)
+        return true;
+    else 
+        return false;
+}
+
+int main(int argc, char const *argv[])
+{
+    int result = 0;
+    int max = 0;
+    for (int i = 999; i >= 100; --i)
+    {
+        for (int j = 999; j >= 100; --j)
+        {
+            result = i * j;
+            if(isPaline(result)) {
+                if(max < result) {
+                    max = result;
+                }
+            }
+        }
+        
+    }
+    
+    cout << max << endl;
+    return 0;
+}

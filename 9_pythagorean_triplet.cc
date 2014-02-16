8f5cc1dc3a48f893a6d65ba9384f47a0cf7e5634
Daniel-Xu <xwd.forever@gmail.com>
2014-02-16 02:12:48 -0500

    cannot figure out right now

---
+++ 9_pythagorean_triplet.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    // (a+b+c)2 = a2 + b2 + c2 + 2ab+2ac+2bc
+    // = 2(c2 + bc + ac + ab)
+    // = 2(c(b+c) + a(b+c))
+    // = 2((b+c)(a+c))
+    // = 2((1000-a)(1000-b))
+
+    return 0;
+}

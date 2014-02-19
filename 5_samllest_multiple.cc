a6c294a72c57dda7a4061e86d609840c5ba829ba
Daniel-Xu <xwd.forever@gmail.com>
2014-02-18 23:17:46 -0500

    after refactoring
    ./app  0.00s user 0.00s system 64% cpu 0.005 total
    
    effect is dramatic

--- 5_samllest_multiple.cc
+++ 5_samllest_multiple.cc
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 using namespace std;
 
@@ -19,15 +20,53 @@ bool div(int num)
 
 int main(int argc, char const *argv[])
 {
-    int i = 2520;
-    while(1) {
-        if(div(i)){
-            cout << "result is: "  << i << endl;
-            return i;
-        }
-        else 
-            i++;
-    }
 
+    //result = 2^4 * 3^2 * 5^1 * 7^1 * 
+    //prime factor that less than 20 should all be contained
+    
+
+    //the exponent of 2, 3, 5, should match the express:   x^num < 20
+    //for example 2^4 < 20, 2^5>20, so num = 4
+    //a[i] = floor( log(k) / log(p[i]) ).
+
+    //x^2 > 20 ---> x <= sqrt(20)
+    //pow
+
+    //int i = 2520;
+    //while(1) {
+        //if(div(i)){
+            //cout << "result is: "  << i << endl;
+            //return i;
+        //}
+        //else 
+            //i++;
+    //}
+
+    //int p = {2, 3, 5, 7, 9, 11, 13, 17, 19};
+    //              ->limit
+
+    int k = 20;
+    int p[] = {2, 3, 5, 7, 11, 13, 17, 19};
+    int i = 0;
+    int limit = sqrt(k);
+    int result = 1;
+    int exp = 1;
+    bool flag =true;
+
+    while(i < 8) {
+        exp = 1;
+
+        if(flag) {
+            if(p[i] <= limit){
+                exp = floor(log(k)/log(p[i]));
+            } else {
+                flag = false;
+            }
+        } 
+
+        result = result* pow(p[i], exp);
+        i++;
+    }
+    cout << result << endl;
     return 0;
 }
